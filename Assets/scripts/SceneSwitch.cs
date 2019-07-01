13e66d1b321f79f6818209c02397aadedb964d60
Jacob Bluhm <bluhmj96@gmail.com>
2019-07-01 12:55:51 -0400

    Adding Comments

--- Assets/scripts/SceneSwitch.cs
+++ Assets/scripts/SceneSwitch.cs
@@ -2,28 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+/*
+ *  A class that controls scene switching 
+ */
 public class SceneSwitch
 {
     public int sceneIndex;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    /*
+     * switch scene to index. The index for scences are set in build settings
+     */
     public void switchScenes(int index)
     {
         sceneIndex = index;
         SceneManager.LoadScene(index);
     }
 
+    /*
+     * Get Current scene index.
+     */
     public int getSceneIndex()
     {
         return sceneIndex;
